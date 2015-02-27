933864f968f4d130cd7c778b6ae26d0cb45d3fe8
J. Antognini <antognini@astronomy.ohio-state.edu>
2015-02-27 16:38:02 -0500

    Added semi-analytic calculation of the period
    This function semi-analytically calculates the time between flips (or
    otherwise the period of oscillations in j_z or Omega if the triple doesn't
    flip.)  The period calculation for non-flipping systems may not be accurate;
    this functionality has not yet been tested.
    
    I additionally added a function to calculate whether the triple flips.

--- ekm.py
+++ ekm.py
@@ -13,7 +13,8 @@ import sys
 import time
 import random
 import numpy as np
-from scipy.integrate import ode
+from scipy.integrate import ode, quad
+from scipy.optimize import brentq
 from scipy.special import ellipk, ellipe
 import astropy.constants as const
 import astropy.units as unit
@@ -71,6 +72,7 @@ class Triple_octupole:
       self.epsoct = self.e2 / (1 - self.e2**2) * (self.a1 / self.a2)
     else:
       self.epsoct = epsoct
+    self.chi = self.F(self.CKL) - self.epsoct * np.cos(self.Omega)
     self.set_x()
     self.set_fj()
     self.set_fOmega()
@@ -166,6 +168,54 @@ class Triple_octupole:
 
     '''
     print self.t, self.jz, self.Omega, self.fj, self.fOmega, self.x, self.CKL
+
+  def _F_integrand(self, x):
+    return (ellipk(x) - 2 * ellipe(x)) / (41*x - 21) / np.sqrt(2*x + 3)
+
+  def F(self, CKL):
+    x_low = (3 - 3 * CKL) / (3 + 2 * CKL)
+    integral = quad(self._F_integrand, x_low, 1)[0]
+    return 32 * np.sqrt(3) / np.pi * integral
+
+  def period(self):
+    '''Analytically calculate the period of EKM oscillations.'''
+
+    # First calculate the limits. 
+    CKLmin = brentq(lambda CKL: self.chi - self.epsoct - self.F(CKL), 0, self.Xi)
+    if self.doesflip():
+      CKLmax = self.Xi
+    else:
+      CKLmax = brentq(lambda CKL: self.chi + self.epsoct - self.F(CKL), 0, 1)
+
+    print CKLmin, CKLmax
+
+    prefactor = 256 * np.sqrt(10) / (15 * np.pi) / self.epsoct
+    P = quad(lambda CKL: (prefactor * ellipk((3 - 3*CKL)/(3 + 2*CKL)) / 
+      (4 - 11*CKL) / np.sqrt(6 + 4*CKL) / np.sqrt(1 - 1/self.epsoct**2 *
+      (self.F(CKL) - self.chi)**2) / np.sqrt(2* np.fabs(self.Xi - CKL))), 
+      CKLmin, CKLmax, epsabs=1e-12, epsrel=1e-12, limit=100)
+
+    return P[0]
+
+  def doesflip(self):
+    '''Return True if the triple flips, false otherwise.  This is determined
+    using Eq. 16 of Katz (2011).
+    '''
+
+    #
+    # Calculate Delta F over many values of x.  x can range from
+    #
+    # C_KL < x < C_KL + j_z^2 / 2
+    #
+    X = np.linspace(self.CKL, self.CKL + (self.jz)**2 / 2.)
+    DeltaF = np.fabs(np.array(map(self.F, X)) - self.F(self.CKL))
+
+    epsoct_crit = np.max(DeltaF) / 2.
+
+    if self.epsoct > epsoct_crit:
+      return True
+    else:
+      return False
 
 if __name__ == '__main__':
   triple = Triple_octupole(epsoct=.01, tstop=400)
