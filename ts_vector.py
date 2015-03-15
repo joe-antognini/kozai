29deeb7dd07bc7b22a904754a83984399cb33322
J. Antognini <joe.antognini@gmail.com>
2015-03-14 21:31:47 -0400

    Added flip_time function to ts_vector
    The flip_time function prints out only the times during which a triple
    flips.  This makes it easy to calculate the period of the EKM using the
    secular code.
    
    I also added a function which resets the triple back to time zero so that it
    can be integrated multiple times.  This does not yet work properly because
    the integrator itself is not reset, only the variables in the class.

--- ts_vector.py
+++ ts_vector.py
@@ -15,41 +15,44 @@ class Triple_vector:
     # Given parameters
     self.a1 = a1
     self.a2 = a2
-    self.e1 = e1
-    self.e2 = e2
-    self.inc = inc * np.pi / 180
-    self.Omega = longascnode * np.pi / 180
-    self.omega = argperi * np.pi / 180
+    self.e1_0 = e1
+    self.e2_0 = e2
+    self.inc_0 = inc * np.pi / 180
+    self.Omega_0 = longascnode * np.pi / 180
+    self.omega_0 = argperi * np.pi / 180
     self.m1 = m1
     self.m3 = m3
 
     # Derived parameters
-    self.j = np.sqrt(1 - self.e1**2)
+    self.j_0 = np.sqrt(1 - self.e1_0**2)
 
     if epsoct is None:
       self.epsoct = (self.a1 / self.a2) * self.e2 / (1 - self.e2**2)
     else:
       self.epsoct = epsoct
+      self.e2_0 = None
+      self.a1 = None
+      self.a2 = None
 
     # The vectorial elements
-    self.jhatvec = np.array([
-      sin(self.inc) * sin(self.Omega),
-      -sin(self.inc) * cos(self.Omega),
-      cos(self.inc)])
-    self.jvec = self.j * self.jhatvec
+    self.jhatvec_0 = np.array([
+      sin(self.inc_0) * sin(self.Omega_0),
+      -sin(self.inc_0) * cos(self.Omega_0),
+      cos(self.inc_0)])
+    self.jvec_0 = self.j_0 * self.jhatvec_0
 
-    ehatvec_sol = root(_evec_root, [.5, .5, .5], (self.jhatvec, self.omega))
-    self.ehatvec = ehatvec_sol.x
-    self.evec = self.e1 * self.ehatvec
+    ehatvec_sol = root(_evec_root, [.5, .5, .5], (self.jhatvec_0, self.omega_0))
+    self.ehatvec_0 = ehatvec_sol.x
+    self.evec_0 = self.e1_0 * self.ehatvec_0
 
     # Elements of the potential
-    self.Phi0 = 4 * np.pi**2 * self.m3 * self.a1**2 / (self.a2**3 * (1 -
-      self.e2**2)**(3/2.))
-    self.phiq = 3/4. * (self.jvec[2]**2 / 2. + self.e1**2 - 5/2. *
-      self.evec[2]**2 - 1/6.)
-    self.phioct = self.epsoct * 75/64. * (self.evec[0] * (1/5. - 8/5. * 
-      self.e1**2 + 7 * self.evec[2]**2 - self.jvec[2]**2) - 2 * self.evec[2]
-      * self.jvec[0] * self.jvec[2])
+#    self.Phi0 = 4 * np.pi**2 * self.m3 * self.a1**2 / (self.a2**3 * (1 -
+#      self.e2**2)**(3/2.))
+    self.phiq = 3/4. * (self.jvec_0[2]**2 / 2. + self.e1_0**2 - 5/2. *
+      self.evec_0[2]**2 - 1/6.)
+    self.phioct = self.epsoct * 75/64. * (self.evec_0[0] * (1/5. - 8/5. * 
+      self.e1_0**2 + 7 * self.evec_0[2]**2 - self.jvec_0[2]**2) - 2 *
+      self.evec_0[2] * self.jvec_0[0] * self.jvec_0[2])
 
     # Integration parameters
     self.nstep = 0
@@ -59,7 +62,11 @@ class Triple_vector:
     self.outfreq = outfreq
     self.outfilename = outfilename
     self.integration_algo = 'vode'
-    self.y = list(np.concatenate((self.jvec, self.evec)))
+    self.y = list(np.concatenate((self.jvec_0, self.evec_0)))
+
+    # We have saved some of the initial values (e.g., jvec_0).  Here we set
+    # them to their respective parameters.  (I.e., we set jvec = jvec_0[:].)
+    self._set_params()
 
     if self.outfilename is not None:
       self.outfile = open(self.outfilename, 'w')
@@ -70,6 +77,30 @@ class Triple_vector:
       rtol=rtol)
     self.solver.set_initial_value(self.y, self.t).set_f_params(self.epsoct)
     self.solver._integrator.iwork[2] = -1 # Don't print FORTRAN errors
+
+  def _set_params(self):
+    '''Set the variables to their initial values.  Just a clone of
+    reset().'''
+    self.reset()
+
+  def reset(self):
+    '''Set the variables to their initial values.'''
+
+    # Ordinary variables
+    self.e1 = self.e1_0
+    self.e2 = self.e2_0
+    self.inc = self.inc_0
+    self.Omega = self.Omega_0
+    self.omega = self.omega_0
+    self.j = self.j_0
+    self.nstep = 0
+    self.t = 0
+
+    # Arrays need to be deep copied
+    self.jhatvec = self.jhatvec_0[:]
+    self.jvec = self.jvec_0[:]
+    self.ehatvec = self.ehatvec_0[:]
+    self.evec = self.evec_0[:]
 
   def _deriv(self, t, y, epsoct):
     '''The EOMs.  See Eqs. 4 of Katz et al. (2011).'''
@@ -131,6 +162,21 @@ class Triple_vector:
       print outstring
     else:
       self.outfile.write(outstring + '\n')
+
+  def flip_times(self, nflips=3):
+    '''Find the times that the inner binary flips.'''
+    sign = np.sign(self.jvec[2])
+    sign_prev = sign
+    flip_count = 0
+
+    # Integrate along...
+    while flip_count < nflips:
+      self._step()
+      sign = np.sign(self.jvec[2])
+      if sign != sign_prev:
+        flip_count += 1
+        self.printout()
+      sign_prev = sign
 
   def __exit__(self):
     self.outfile.close()
