3c879c30813be2352de433b70b99c95467d7b3e2
Joe Antognini <joe.antognini@gmail.com>
2020-05-09 22:47:07 -0700

    Fix formatting in kl_period.py.

--- kozai/kl_period.py
+++ kozai/kl_period.py
@@ -1,13 +1,10 @@
 """Code to calculate timescales in a hierarchical triple."""
 
-import json
 import time
-from math import sqrt, cos
+from math import sqrt
 
 import numpy as np
 from scipy.integrate import quad
-
-from _kozai_constants import *
 
 
 def P_out(triple):
@@ -21,22 +18,22 @@ def P_in(triple):
 
 
 def kl_period_oom(triple):
-    """The usual KL period formula:
+    """Calculate the usual KL period formula:
 
-  t_KL = P_out^2 / P_in * (1 - e2^2)^(3/2)
+    t_KL = P_out^2 / P_in * (1 - e2^2)^(3/2)
 
-  Parameters:
-    triple: A Triple object
+    Parameters:
+      triple: A Triple object
 
-  Returns:
-    P: The period of KL oscillations in years.
+    Returns:
+      P: The period of KL oscillations in years.
 
-  """
+    """
 
     # fmt: off
     return (
         8 / (15 * np.pi) * (1 + triple.m1 / triple.m3) * P_out(triple)**2
-        / P_in(triple) * (1 - triple.e2**2)**(3.0 / 2)
+        / P_in(triple) * (1 - triple.e2**2)**(3 / 2)
     )
     # fmt: on
 
@@ -44,24 +41,23 @@ def kl_period_oom(triple):
 def is_librating(triple):
     """Determine whether the triple is librating or rotating.
 
-  Parameters:
-    triple: A Triple object
+    Parameters:
+        triple: A Triple object
 
-  Returns:
-    True if librating, False if rotating.
-  """
+    Returns:
+        True if librating, False if rotating.
 
+    """
     return triple.CKL <= 0
 
 
 def depsdh(eps, H, Th):
     """Calculate the derivative of epsilon with respect to H."""
-    
     # fmt: off
     return (
         eps**2 / ((1 - eps**2) * (eps**2 - Th) *
         sqrt(1 - (3 * eps**4 + eps**2 * (H - 9 * Th - 5) + 15 * Th)**2 /
-        (225 * (1 - eps**2)**2 * (eps**2 - Th)**2))
+        (225 * (1 - eps**2)**2 * (eps**2 - Th)**2)))
     )
     # fmt: on
 
@@ -84,14 +80,13 @@ def kl_period_norm(Hhat, Th):
 def kl_period(triple):
     """Calculate the period of KL oscillations semi-analytically.
 
-  Parameters:
-    triple: A Triple object
+    Parameters:
+        triple: A Triple object
 
-  Returns:
-    P: The period in years
+    Returns:
+        P: The period in years
 
-  """
-
+    """
     # fmt: off
     L1toC2 = (
         16 * triple.a2 * (1 - triple.e2**2)**(3 / 2.0) / triple.m3 *
@@ -105,16 +100,16 @@ def kl_period(triple):
 def numeric_kl_period(triple, nperiods=3, tstop=1e6):
     """Calculate the KL period using the secular equations of motion.
 
-  This will explicitly numerically integrate the secular equations of motion.
+    This will explicitly numerically integrate the secular equations of motion.
 
-  Input:
-    triple: A triple class
-    n_periods: (optional) The number of KL cycles over which to integrate
+    Input:
+        triple: A triple class
+        n_periods: (optional) The number of KL cycles over which to integrate
 
-  Output:
-    The average period of KL oscillations in yr.
+    Output:
+        The average period of KL oscillations in yr.
 
-  """
+    """
 
     triple.tstop = tstop
     triple.integrator_setup()
